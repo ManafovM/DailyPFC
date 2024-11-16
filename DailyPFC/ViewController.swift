1857e4bfc228cdf892029d17c09e2dd7eb960ff6
marif.manafov <marif.manafov@aijus.com>
2024-11-16 14:57:54 +0900

    Initial Commit

---
+++ DailyPFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPFC
+//
+//  Created by マナフォフ・マリフ on 2024/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DailyPFCTests/DailyPFCTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPFCTests.swift
+//  DailyPFCTests
+//
+//  Created by マナフォフ・マリフ on 2024/11/16.
+//
+
+import Testing
+@testable import DailyPFC
+
+struct DailyPFCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
